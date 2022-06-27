22316c7c62aa62d01db850fc30ec188c22c11046
Connie Park <conniedc1206@gmail.com>
2022-06-27 14:49:24 -0700

    commit tree 93492e35aabecd28eb41abc680292f147246ee20

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    # The first method (setter) takes in an argument of a dog's name and assigns that value to a variable, this_dogs_name
+    # define an instance variable by prefacing the variable name with an @ symbol.
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
 
+    # The second method (getter) is responsible for reporting the name
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
